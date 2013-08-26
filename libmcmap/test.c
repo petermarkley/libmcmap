8ece1f43d5c7c04ccda0539d4132aa3bcdab002e
Peter Markley <peter@malexmedia.net>
2013-08-26 19:36:19 +0000

    btcmines: libmcmap: make test application a little more dynamic about picking which chunk to read

--- libmcmap/test.c
+++ libmcmap/test.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv)
 	{
 	FILE *f;
 	struct mcmap_region *reg;
+	unsigned int x,z;
+	z = 0; x = 1;
 	
 	if ((reg = mcmap_read_region(0,0,"./test_map/saves/Experiment Lab/")) == NULL)
 		{
@@ -12,17 +14,18 @@ int main(int argc, char **argv)
 		return -1;
 		}
 	
-	fprintf(stderr,"sector offset for chunk 0,0: %d (derived from %02x %02x %02x)\n",reg->locations[0][0],reg->header->locations[0][0].offset[0],reg->header->locations[0][0].offset[1],reg->header->locations[0][0].offset[2]);
-	fprintf(stderr,"sector count for chunk 0,0: %x\n",reg->header->locations[0][0].sector_count);
-	fprintf(stderr,"pointer was accordingly set to %x (%d bytes after beginning at %x)\n",(unsigned int)(reg->chunks[0][0].header),(unsigned int)(reg->chunks[0][0].header)-(unsigned int)(reg->header),(unsigned int)(reg->header));
-	fprintf(stderr,"chunk size in bytes: %u\n",reg->chunks[0][0].size);
+	fprintf(stderr,"stats for chunk (%u,%u):\n",x,z);
+	fprintf(stderr,"\tsector offset: %u (derived from %02x %02x %02x)\n",reg->locations[z][x],reg->header->locations[z][x].offset[0],reg->header->locations[z][x].offset[1],reg->header->locations[z][x].offset[2]);
+	fprintf(stderr,"\tsector count: %x\n",reg->header->locations[z][x].sector_count);
+	fprintf(stderr,"\tpointer was accordingly set to %x (%u bytes after beginning at %x)\n",(unsigned int)(reg->chunks[z][x].header),(unsigned int)(reg->chunks[z][x].header)-(unsigned int)(reg->header),(unsigned int)(reg->header));
+	fprintf(stderr,"\tchunk size in bytes: %u\n",reg->chunks[z][x].size);
 	
 	if ((f = fopen("./test-parsed_chunk.nbt","w")) == NULL)
 		{
 		fprintf(stderr,"couldn't write chunk for some reason\n");
 		return -1;
 		}
-	fwrite(reg->chunks[0][0].data,1,reg->chunks[0][0].size,f);
+	fwrite(reg->chunks[z][x].data,1,reg->chunks[z][x].size,f);
 	fclose(f);
 	
 	mcmap_free_region(reg);
