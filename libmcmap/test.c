adf46df74576e2db963709e3ffaa6e29a8504e89
Peter Markley <peter@malexmedia.net>
2013-09-05 18:47:22 +0000

    btcmines: libmcmap: point test case to new map, make room in variable names for some variables i commonly add for testing

--- libmcmap/test.c
+++ libmcmap/test.c
@@ -3,13 +3,13 @@
 #include "./mcmap.h"
 #include "./libnbt/nbt.h"
 
-#define INP_MAP "./test_map/saves/Experiment Lab/"
+#define INP_MAP "./test_map/saves/New World/"
 #define OUT_FIL "./test-parsed_chunk.nbt"
 
 int main(int argc, char **argv)
 	{
 	struct mcmap_region *r;
-	unsigned int x,z;
+	unsigned int cx,cz;
 	struct mcmap_chunk *c;
 	
 	if ((r = mcmap_region_read(0,0,INP_MAP)) == NULL)
@@ -18,13 +18,13 @@ int main(int argc, char **argv)
 		return -1;
 		}
 	
-	z = 0; x = 1;
-	if (r->chunks[z][x].header != NULL)
+	cz = 0; cx = 0;
+	if (r->chunks[cz][cx].header != NULL)
 		{
 		//extract a single chunk and pass it to libnbt!
-		c = mcmap_chunk_read(&(r->chunks[z][x]),MCMAP_READ_FULL,1);
+		c = mcmap_chunk_read(&(r->chunks[cz][cx]),MCMAP_READ_FULL,1);
 		mcmap_region_free(r);
-		fprintf(stdout,"\nNBT data from chunk (%u,%u), decrompressed from %u bytes and last updated %s\n",x,z,(unsigned int)r->chunks[z][x].size,ctime(&(r->dates[z][x])));
+		fprintf(stdout,"\nNBT data from chunk (%u,%u), decrompressed from %u bytes and last updated %s\n",cx,cz,(unsigned int)r->chunks[cz][cx].size,ctime(&(r->dates[cz][cx])));
 		nbt_print_ascii(stdout,c->raw,3,10);
 		fprintf(stdout,"\n");
 		mcmap_chunk_free(c);
