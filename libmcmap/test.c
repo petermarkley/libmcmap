f9ce34e127545b1eab6ae8c3aa4bb492a813e0e0
Peter Markley <peter@malexmedia.net>
2013-10-11 03:06:01 +0000

    btcmines: libmcmap: make a cooler test scenario

--- libmcmap/test.c
+++ libmcmap/test.c
@@ -10,14 +10,16 @@
 
 #define OUT_MAP "./maps/My Super Test/"
 #define MAX_STR 1024
+#define PI 3.14159265358979323
 
 int main(int argc, char **argv)
 	{
 	struct mcmap_level *l;
-	int x,y,z;
+	int x,y,z, bottom, mid;
+	double start_phase, end_phase, width, lrad, max, min;
 	
 	//initialize map
-	if ((l = mcmap_level_new(0,"My Super Test","flat","0",0,MCMAP_GAME_ADVENTURE,0,1,1,0,1,1,1,1,0,0,1,0,64,0,OUT_MAP)) == NULL)
+	if ((l = mcmap_level_new(0,"My Super Test","flat","0",0,MCMAP_GAME_CREATIVE,0,1,1,0,1,1,1,1,0,0,1,0,41,0,OUT_MAP)) == NULL)
 		{
 		fprintf(stderr,"%s: %s\n",MCMAP_LIBNAME,mcmap_error);
 		return -1;
@@ -28,14 +30,58 @@ int main(int argc, char **argv)
 		return -1;
 		}
 	
-	//populate map
-	for (y=63;y>=0;y--)
+	//create floating island
+	mid = 40; bottom = 25; width = 20.0;
+	start_phase = 90.0; end_phase = 260.0;
+	for (y=mid;y>=bottom;y--)
+		{
+		max = sin(start_phase*PI/180.0);
+		min = sin(end_phase*PI/180.0);
+		lrad = ((sin(   (( (((double)mid)-((double)y))/((double)(mid-bottom)) ) * (end_phase-start_phase)+start_phase)*PI/180.0   )-min)/(max-min))*width;
+		for (z=-40;z<40;z++)
+			{
+			for (x=-40;x<40;x++)
+				{
+				if (sqrt(pow(x,2)+pow(z,2)) < lrad)
+					mcmap_set_block(&(l->overworld),x,y,z,MCMAP_STONE);
+				}
+			}
+		}
+	mid = 44; bottom = 41; width = 20.0;
+	start_phase = 00.0; end_phase = 90.0;
+	for (y=mid;y>=bottom;y--)
+		{
+		max = sin(end_phase*PI/180.0);
+		min = sin(start_phase*PI/180.0);
+		lrad = ((sin(   (sin(( (((double)mid)-((double)y))/((double)(mid-bottom)) )*90.0*PI/180.0) * (end_phase-start_phase)+start_phase)*PI/180.0   )-min)/(max-min))*width;
+		for (z=-40;z<40;z++)
+			{
+			for (x=-40;x<40;x++)
+				{
+				if (sqrt(pow(x,2)+pow(z,2)) < lrad)
+					mcmap_set_block(&(l->overworld),x,y,z,MCMAP_DIRT);
+				}
+			}
+		}
+	for (y=mid;y>=bottom;y--)
 		{
 		for (z=-40;z<40;z++)
 			{
 			for (x=-40;x<40;x++)
-				mcmap_set_block(&(l->overworld),x,y,z,MCMAP_GLASS);
+				{
+				if (mcmap_get_block(&(l->overworld),x,y,z) == MCMAP_DIRT && mcmap_get_block(&(l->overworld),x,y+1,z) == MCMAP_AIR)
+					{
+					mcmap_set_block(&(l->overworld),x,y,z,MCMAP_GRASS);
+					mcmap_set_block(&(l->overworld),x,y+1,z,MCMAP_TALL_GRASS);
+					mcmap_set_data(&(l->overworld),x,y+1,z,0x01);
+					}
+				}
 			}
+		}
+	for (z=-40;z<40;z++)
+		{
+		for (x=-40;x<40;x++)
+			mcmap_set_biome(&(l->overworld),x,z,MCMAP_PLAINS);
 		}
 	
 	//save map
