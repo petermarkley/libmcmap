a61990fc6eb39b107e19fe2e482353fc013e6b91
Peter Markley <peter@malexmedia.net>
2016-05-30 13:39:47 +0000

    btcmines: libmcmap: shownbt: apparently i did a super sloppy job on this before, and somehow got away with it? lolz

--- libmcmap/shownbt.c
+++ libmcmap/shownbt.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 	{
 	struct nbt_tag *t;
 	int i,j,v=DEFHGT,w=DEFWID,done;
-	int infile[argc], x,z, rx,rz, cx,cz;
+	int infile=-1, x,z, rx,rz, cx,cz;
 	struct mcmap_region *r;
 	char ret = '\0';
 	char fn[STRMAX];
@@ -27,7 +27,6 @@ int main(int argc, char **argv)
 		{
 		if (argv[i][0] == '-')
 			{
-			infile[i] = 0;
 			done = 0;
 			for (j=1; !done && argv[i][j] != '\0'; j++)
 				{
@@ -36,7 +35,7 @@ int main(int argc, char **argv)
 					case 'v': v=-1; break;
 					case 'q': v= 0; break;
 					case 'w':
-						infile[++i] = 0;
+						i++;
 						if (sscanf(argv[i],"%d",&w) != 1)
 							{
 							fprintf(stderr,"couldn't parse '%s %s'\n",argv[i-1],argv[i]);
@@ -68,64 +67,61 @@ int main(int argc, char **argv)
 				}
 			}
 		else
-			infile[i] = 1;
+			infile = i;
 		}
 	//read and print
-	for (i=1; i<argc; i++)
+	i = infile;
+	if (argv[i] > 0)
 		{
-		if (infile[i])
+		if (ret != '\0')
 			{
-			if (ret != '\0')
+			//resolve filename
+			for (j=0;argv[i][j+1]!='\0';j++);
+			switch (ret)
 				{
-				//resolve filename
-				for (j=0;argv[i][j+1]!='\0';j++);
-				switch (ret)
-					{
-					case 'o':
-						snprintf(fn,STRMAX,"%s%sregion/",argv[i],(argv[i][j]=='/'?"":"/"));
-						break;
-					case 'n':
-						snprintf(fn,STRMAX,"%s%sDIM-1/region/",argv[i],(argv[i][j]=='/'?"":"/"));
-						break;
-					case 'e':
-						snprintf(fn,STRMAX,"%s%sDIM1/region/",argv[i],(argv[i][j]=='/'?"":"/"));
-						break;
-					default: break;
-					}
-				//resolve coordinates
-				rx = (int)floor(((double)x)/32.0);
-				rz = (int)floor(((double)z)/32.0);
-				cx = ( (x<0) ? ((x+1)%32+31) : (x%32) );
-				cz = ( (z<0) ? ((z+1)%32+31) : (z%32) );
-				//read data
-				if ((r = mcmap_region_read(rx,rz,fn)) == NULL)
-					{
-					fprintf(stderr,"%s: %s\n",MCMAP_LIBNAME,mcmap_error);
-					return -1;
-					}
-				if (r->chunks[cz][cx].data == NULL)
-					{
-					fprintf(stderr,"relative chunk (%d,%d) doesn't exist in %s\n",cx,cz,fn);
-					return 0;
-					}
-				if ((t = nbt_decode(r->chunks[cz][cx].data,r->chunks[cz][cx].size,NBT_COMPRESS_UNKNOWN)) == NULL)
-					{
-					fprintf(stderr,"%s: %s\n",NBT_LIBNAME,nbt_error);
-					return -1;
-					}
-				//free unnessecary data
-				mcmap_region_free(r);
+				case 'o':
+					snprintf(fn,STRMAX,"%s%sregion/",argv[i],(argv[i][j]=='/'?"":"/"));
+					break;
+				case 'n':
+					snprintf(fn,STRMAX,"%s%sDIM-1/region/",argv[i],(argv[i][j]=='/'?"":"/"));
+					break;
+				case 'e':
+					snprintf(fn,STRMAX,"%s%sDIM1/region/",argv[i],(argv[i][j]=='/'?"":"/"));
+					break;
+				default: break;
 				}
-			else
-				if ((t = nbt_file_read(argv[i])) == NULL)
-					{
-					fprintf(stderr,"%s: %s\n",NBT_LIBNAME,nbt_error);
-					return -1;
-					}
+			//resolve coordinates
+			rx = (int)floor(((double)x)/32.0);
+			rz = (int)floor(((double)z)/32.0);
+			cx = ( (x<0) ? ((x+1)%32+31) : (x%32) );
+			cz = ( (z<0) ? ((z+1)%32+31) : (z%32) );
+			//read data
+			if ((r = mcmap_region_read(rx,rz,fn)) == NULL)
+				{
+				fprintf(stderr,"%s: %s\n",MCMAP_LIBNAME,mcmap_error);
+				return -1;
+				}
+			if (r->chunks[cz][cx].data == NULL)
+				{
+				fprintf(stderr,"relative chunk (%d,%d) doesn't exist in %s\n",cx,cz,fn);
+				return 0;
+				}
+			if ((t = nbt_decode(r->chunks[cz][cx].data,r->chunks[cz][cx].size,NBT_COMPRESS_UNKNOWN)) == NULL)
+				{
+				fprintf(stderr,"%s: %s\n",NBT_LIBNAME,nbt_error);
+				return -1;
+				}
+			//free unnessecary data
+			mcmap_region_free(r);
 			}
+		else
+			if ((t = nbt_file_read(argv[i])) == NULL)
+				{
+				fprintf(stderr,"%s: %s\n",NBT_LIBNAME,nbt_error);
+				return -1;
+				}
 		nbt_print_ascii(stdout,t,v,w);
+		nbt_free(t);
 		}
-	//clean up
-	nbt_free(t);
 	return 0;
 	}
